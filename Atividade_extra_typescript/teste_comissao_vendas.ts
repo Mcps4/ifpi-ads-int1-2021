77dc06c19ebfb911b6e0748571f0d78e0b759111
Mcps4 <61793220+Mcps4@users.noreply.github.com>
2021-07-06 23:47:11 -0300

    Add files via upload

---
+++ Atividade_extra_typescript/teste_comissao_vendas.ts
@@ -0,0 +1,3 @@
+import {calcular_comissao_vendas} from './comissao_vendas'
+let calculo_salario: calcular_comissao_vendas = new calcular_comissao_vendas('joao',3000);
+console.log(calculo_salario.salario_funcinario);
