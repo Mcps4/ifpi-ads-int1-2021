74dd46b541b1fb0bd60d59e929c160abc4762efd
Mcps4 <61793220+Mcps4@users.noreply.github.com>
2021-07-06 23:45:50 -0300

    Delete teste_percertual_covid.ts

--- Atividade_extra_typescript/teste_percertual_covid.ts
+++
@@ -1,3 +0,0 @@
-import {calcular_classificar_dados} from './percentual_covid'
-    let calculo_var: calcular_classificar_dados = new calcular_classificar_dados(20,50);
-    console.log(calculo_var.classificacao_string());
